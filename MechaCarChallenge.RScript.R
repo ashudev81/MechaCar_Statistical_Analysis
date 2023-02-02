a7af468a9448dad5ab18f0f59c55b6fcb177cd82
ashudev81 <ashudev@gmail.com>
2023-02-02 00:01:47 -0500

    adding file

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -12,33 +12,16 @@ total_summary <- summarise(coil_data, Mean = mean(PSI),Median = median(PSI), Var
 lot_grouping <- group_by(coil_data, Manufacturing_Lot) # create data with lot group
 lot_summary <- summarise(lot_grouping, Mean = mean(PSI),Median = median(PSI), Variance = var(PSI), SD = sd(PSI)) # prepare the summary by lot group
 
-# t.test(coil_data$PSI, mu = mean(coil_data$PSI)) # check the mean value accross the all lots
+
 t.test(coil_data$PSI, mu = 1500) # check the mean value accross the all lots
 
-coil_lot1 <- subset(coil_data, coil_data$Manufacturing_Lot == "Lot1") # filter by lot1
-t.test(coil_lot1$PSI, coil_data$PSI)
 
-coil_lot2 <- subset(coil_data, coil_data$Manufacturing_Lot == "Lot2") # filter by lot1
-t.test(coil_lot2$PSI, coil_data$PSI)
+t.test(PSI ~ 1, coil_data, subset = Manufacturing_Lot %in% c('Lot1'), mu= 1500) # check the mean value for lot1
 
-coil_lot3 <- subset(coil_data, coil_data$Manufacturing_Lot == "Lot3") # filter by lot1
-t.test(coil_lot3$PSI, coil_data$PSI)
+t.test(PSI ~ 1, coil_data, subset = Manufacturing_Lot %in% c('Lot2'), mu= 1500) # check the mean value for lot2
 
+t.test(PSI ~ 1, coil_data, subset = Manufacturing_Lot %in% c('Lot3'), mu= 1500) # check the mean value for lot3
 
-# t.test(PSI ~, data = coil_data, subset = Manufacturing_Lot %in% "Lot1")
-#t.test(subset(coil_data, coil_lot1$PSI, coil_data$PSI)
-
-t.test(PSI ~ 1, coil_data, subset = Manufacturing_Lot %in% c('Lot1', 'Lot2', 'Lot3'))
-
-t.test(PSI ~ 1, coil_data, subset = Manufacturing_Lot %in% c('Lot1'))
-
-t.test(PSI ~ 1, coil_data, subset = Manufacturing_Lot %in% c('Lot2'), mu= 1500)
-
-t.test(PSI ~ 1, coil_data, subset = Manufacturing_Lot %in% c('Lot3'), mu= 1500)
-
-t.test(PSI ~ 1, coil_data)
-
-t.test(coil_data$PSI, data=coil_data, Manufacturing_Lot=lot1)
 
 
 
